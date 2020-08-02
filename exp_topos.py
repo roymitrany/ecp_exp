ff793b1ceebbf0ca6ffbe83292f7eeff381e10a6
Roy Mitrany <roym@technion.ac.il>
2020-08-02 15:50:21 +0300

    Integrate with gist file

--- exp_topos.py
+++ exp_topos.py
@@ -14,49 +14,77 @@ from mininet.topo import Topo
 from mininet.node import Node
 import random
 
+"""*************************************************************
+                     Hosts
+****************************************************************"""
+
+
+# This type of host has routing capabilities
+
+class LinuxRouter(Node):
+    "A Node with IP forwarding enabled."
+
+    def config(self, **params):
+        super(LinuxRouter, self).config(**params)
+        # Enable forwarding on the router
+        self.cmd('sysctl net.ipv4.ip_forward=1')
+
+    def terminate(self):
+        self.cmd('sysctl net.ipv4.ip_forward=0')
+        super(LinuxRouter, self).terminate()
+
+
+# This type of host runs nuttcp server
+
+class NuttcpResponder(Node):
+    "A Node listening as NUTTCP server (port 5000)."
+
+    def config(self, **params):
+        super(NuttcpResponder, self).config(**params)
+
+        self.cmd('sysctl net.ipv4.tcp_rmem="400000 400000 400000"')  # Avoid RWin scale (confuses Wireshark)
+        self.cmd('sysctl net.ipv4.neigh.{}-eth0.gc_stale_time=720000'.format(self.name))
+        self.cmd('arp -s 10.60.0.100 00:00:00:00:01:00')  # Static arp cache from responder to sender
+        self.cmd('ethtool -K {}-eth0 tso off'.format(self.name))  # Disable GSO/TSO
+        self.cmd('ethtool -K {}-eth0 gso off'.format(self.name))
+        self.cmd('nuttcp -S')
+
+    def set_sack(self, sack_val):
+        self.cmd('sysctl net.ipv4.tcp_sack={}'.format(sack_val))
+
+
+class NuttcpClient(Node):
+
+    def config(self, **params):
+        super(NuttcpClient, self).config(**params)
+
+        self.cmd('sysctl net.ipv4.neigh.{}-eth0.gc_stale_time=720000'.format(self.name))  # Extend ARP cache expiration
+        self.cmd('ethtool -K {}-eth0 tso off'.format(self.name))  # Disable GSO/TSO
+        self.cmd('ethtool -K {}-eth0 gso off'.format(self.name))
+
+
 class PingerNode(Node):
     "A Node with IP forwarding enabled."
+
     def config(self, **params):
         super(PingerNode, self).config(**params)
-        
-	self.cmd('python3 random_pinger.py 10.69.0 1 20&')
+
+        self.cmd('python3 random_pinger.py 10.69.0 1 20&')
 
     def terminate(self):
         super(PingerNode, self).terminate()
 
-class ArpTopo(Topo):
-    def build(self, **_opts):
-        s1 = self.addSwitch('s1')
-        my_host = self.addHost('myhost',ip="10.69.0.100/24")
-        self.addLink(s1, my_host, cls=TCLink)
 
-        for i in range(1,21):
-            dst = self.addHost('dst%d'%i,cls=PingerNode,ip="10.69.0.%d/24"%i)
-            self.addLink(s1, dst, cls=TCLink)
+"""*************************************************************
+                     Topologies
+****************************************************************"""
 
-class ExpTopo(Topo):
-    def __init__(self):
-
-        # super should be called only after class member initialization.
-        # The super constructor calls build.
-        super(ExpTopo, self).__init__()
-
-    def build(self, **_opts):
-        s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
-        self.student_desktop = self.addHost('sd')
-        self.close_responder = self.addHost('cr')
-        self.remote_responder = self.addHost('rr')
-        self.addLink(s1, s2, cls = TCLink, bw=1, delay='250ms',max_queue_size=100)
-        self.addLink(s1, self.student_desktop, cls = TCLink)
-        self.addLink(s1, self.close_responder, cls = TCLink)
-        self.addLink(s2, self.remote_responder,cls = TCLink)
 
 class IntroTopo(Topo):
     def build(self, **_opts):
         s1 = self.addSwitch('s1')
         my_host = self.addHost('myhost',ip="10.69.0.100/24")
-        close_dst = self.addHost('dst1',ip="10.69.0.1/24")        
+        close_dst = self.addHost('dst1',ip="10.69.0.1/24")
 	far_dst = self.addHost('dst2',ip="10.69.0.2/24")
 	lossy_dst = self.addHost('dst3',ip="10.69.0.3/24")
 
@@ -66,4 +94,81 @@ class IntroTopo(Topo):
         self.addLink(s1, lossy_dst,cls=TCLink, bw=1, loss=50)
 
 
-topos = { 'expTopo':ExpTopo, 'introTopo':IntroTopo, 'arpTopo':ArpTopo }
+
+class ArpTopo(Topo):
+    def build(self, **_opts):
+        s1 = self.addSwitch('s1')
+        my_host = self.addHost('myhost', ip="10.69.0.100/24")
+        self.addLink(s1, my_host, cls=TCLink)
+
+        for i in range(1, 21):
+            dst = self.addHost('dst%d' % i, cls=PingerNode, ip="10.69.0.%d/24" % i)
+            self.addLink(s1, dst, cls=TCLink)
+
+
+class RedirectTopo(Topo):
+    "Topology with 2 routers and 3 subnets"
+
+    def build(self, **_opts):
+        # Define Routers
+        router1 = self.addHost('rtr1', ip="10.69.0.1/24", mac="00:00:00:00:00:11", cls=LinuxRouter,
+                               defaultRoute='via 10.69.0.2')  # Between networks 0 and 1
+        router2 = self.addHost('rtr2', ip="10.69.0.2/24", mac="00:00:00:00:00:22", cls=LinuxRouter,
+                               defaultRoute='via 10.69.0.1')  # Between networks 0 and 2
+
+        # Define hosts with addresses and default routes
+        my_host = self.addHost('myhost', ip="10.69.0.100/24",
+                               defaultRoute='via 10.69.0.1')
+
+        responder1 = self.addHost('responder1', ip="10.69.1.100/24",
+                                  defaultRoute='via 10.69.1.1')
+
+        responder2 = self.addHost('responder2', ip="10.69.2.100/24",
+                                  defaultRoute='via 10.69.2.2')
+
+        # Switches for networks 0,1,2
+        s0 = self.addSwitch('s0')
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+
+        # links between switches to hosts
+        self.addLink(s0, my_host, cls=TCLink)
+        self.addLink(s1, responder1, cls=TCLink)
+        self.addLink(s2, responder2, cls=TCLink)
+
+        # Links between the switches and the routers
+        self.addLink(s0, router1, intfName2='r01-eth1',
+                     params2={'ip': '10.69.0.1/24'})
+
+        self.addLink(s1, router1, intfName2='r01-eth2',
+                     params2={'ip': '10.69.1.1/24'})
+
+        self.addLink(s0, router2, intfName2='r02-eth1',
+                     params2={'ip': '10.69.0.2/24'})
+
+        self.addLink(s2, router2, intfName2='r02-eth2',
+                     params2={'ip': '10.69.2.2/24'})
+
+
+class TcpTopo(Topo):
+    "Topology for meeting 2 TCP protocol analysis"
+
+    def build(self, qSize=None, delay=None, **_opts):
+        # Queue size 0 parameter means None
+        qSize = None if qSize == 0 else qSize
+
+        delay = str(delay) + 'ms' if delay else None
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        my_host = self.addHost('myhost', ip="10.69.0.100/24", mac="00:00:00:00:01:00",
+                               cls=NuttcpClient)
+        close_responder = self.addHost('resp1', ip="10.69.0.1/24", cls=NuttcpResponder)
+        remote_responder = self.addHost('resp2', ip="10.69.0.2/24", cls=NuttcpResponder)
+
+        self.addLink(s1, s2, cls=TCLink, bw=0.5, delay=delay, max_queue_size=qSize, use_tbf=True)
+        self.addLink(s1, my_host, cls=TCLink)
+        self.addLink(s1, close_responder, cls=TCLink)
+        self.addLink(s2, remote_responder, cls=TCLink)
+
+
+topos = {'introTopo': IntroTopo, 'arpTopo': ArpTopo, 'redirectTopo': RedirectTopo, 'tcpTopo': TcpTopo}
